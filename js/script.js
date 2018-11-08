70669b78d394e2971382db9d325bb1e60c848418
Michael Checo <michaeljcheco@gmail.com>
2018-11-08 23:26:01 +0000

    Changes in Javascript

--- js/script.js
+++ js/script.js
@@ -5,7 +5,25 @@
 //GLOBAL VARIABLES
 /* global $ */
 
-
+var randomNumber = 0;
+var userChoice = [];
+var computerChoice = [];
+var winner= [];
 
 // DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
+    $("#userChoice").text($("input").val());
+    randomNumber = Math.floor(Math.random() * 8);
+    userChoice = $("input").val();
+    
+    if(randomNumber <= 2){
+        computerChoice = "Rock";
+    }else if(randomNumber <= 5 && randomNumber > 2){
+        computerChoice = "Paper";
+    }else if(randomNumber <= 6 && randomNumber > 5){
+        computerChoice = "Scissors";
+    }
+    
+});
+
 
